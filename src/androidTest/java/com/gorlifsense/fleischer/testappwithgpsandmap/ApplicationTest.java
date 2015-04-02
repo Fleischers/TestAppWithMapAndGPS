8f143157ce820c773de54b2157691622f29a4125
Glib Fleischer Ischenko <Fleischer458@gmail.com>
2015-04-02 17:55:27 +0300

    Initial project structure

---
+++ src/androidTest/java/com/gorlifsense/fleischer/testappwithgpsandmap/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.gorlifsense.fleischer.testappwithgpsandmap;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
